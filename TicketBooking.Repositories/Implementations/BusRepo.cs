dd3ad128f1d9d746fca2cf80af3c379f422b5d89
Claudia <claudiadurka@gmail.com>
2025-01-10 00:18:02 +0100

    Zaimplementowany BusRepo.

---
+++ TicketBooking.Repositories/Implementations/BusRepo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketBooking.Entities;
+using TicketBooking.Repositories.Interfaces;
+
+namespace TicketBooking.Repositories.Implementations
+{
+    public class BusRepo : IBusRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BusRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Delete(Bus bus)
+        {
+            _context.BusInfo.Remove(bus);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Bus>> GetALL()
+        {
+            return await _context.BusInfo.ToListAsync();
+        }
+
+        public async Task<Bus> GetByID(int id)
+        {
+            return await _context.BusInfo.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Insert(Bus bus)
+        {
+            await _context.BusInfo.AddAsync(bus);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task Update(Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
