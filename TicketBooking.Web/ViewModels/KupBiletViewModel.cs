18b7c266440ae29662a7a71e7692a45a58da0dcf
Claudia <claudiadurka@gmail.com>
2025-01-13 19:47:33 +0100

    KupBiletViewModel

---
+++ TicketBooking.Web/ViewModels/KupBiletViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketBooking.Web.ViewModels
+{
+    public class KupBiletViewModel
+    {
+        public int Id { get; set; }
+        public string KoncertImage { get; set; }
+        public string NazwaKoncertu { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime KoncertDate { get; set; }
+        public List<CheckBoxTable> KupBiletList { get; set; } = new List<CheckBoxTable>();
+    }
+    public class CheckBoxTable 
+    { 
+        public int Id { get; set; }
+        public int NumerMiejsca { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
